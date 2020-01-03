d4d9420c686f0075b33f808a9c75705bb4cddcb0
Ashutosh <bhardwajashu96@hotmail.com>
2020-01-03 10:58:48 +0530

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Raleway-SemiBold", "sans-serif"],
+        body: ["Raleway-Regular", "sans-serif"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
