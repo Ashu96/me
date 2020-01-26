800252aea6146a22fabbdb3df90d1049304b710f
Ashutosh <bhardwajashu96@hotmail.com>
2020-01-26 13:06:29 +0530

    Implement theme toggle

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,24 @@
 /**
- * Layout component that queries for data
+ * Layout component that queries htmlFor data
  * with Gatsby's useStaticQuery component
  *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-// import Header from "./header"
 import "./layout.css"
+import "./toggle.css"
+
+const nightIcon = require("../images/moon.svg")
+const lightIcon = require("../images/contrast.svg")
 
 const Layout = ({ children }) => {
+  const [isThemeDark, setIsThemeDark] = useState(true)
+  const bgColor = isThemeDark ? "bg-gray-900" : "bg-gray-100"
+  const textColor = isThemeDark ? "text-gray-100" : "text-gray-900"
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -27,12 +33,50 @@ const Layout = ({ children }) => {
   return (
     <>
       {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-      <div className='bg-black'>
+      <div className={`${bgColor} ${textColor}`}>
+        <div className="absolute top-0 right-0 flex items-center mt-4 mr-4 py-2">
+          <div className="relative">
+            <input
+              className="tgl tgl-ios"
+              id="toggle-theme"
+              type="checkbox"
+              checked={isThemeDark}
+              onChange={() => setIsThemeDark(!isThemeDark)}
+            />
+            <label
+              className="tgl-btn bg-gray-700"
+              htmlFor="toggle-theme"
+            ></label>
+            <img
+              className={`absolute top-0 ${
+                isThemeDark ? "left-0 ml-2" : "right-0 mr-2"
+              } w-5 mt-1`}
+              src={isThemeDark ? nightIcon : lightIcon}
+              alt="toggle-icon"
+            />
+          </div>
+        </div>
         <main>{children}</main>
-        <footer className="py-4 bg-gray-900 text-gray-100 text-center font-body">
+        <footer className="py-4 text-center font-body">
           © {new Date().getFullYear()}, Built with Love using
           {` `}
-          <a className='text-purple-400 hover:text-purple-700' href="https://www.gatsbyjs.org">Gatsby</a>
+          <a
+            className="text-purple-400 hover:text-purple-700"
+            href="https://www.gatsbyjs.org"
+          >
+            Gatsby
+          </a>
+          <p>
+            Icons made by{" "}
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+              Freepik
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              {" "}
+              www.flaticon.com
+            </a>
+          </p>
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="w-full h-screen p-10 md:px-16 bg-black">
-      <div className='font-body'>
+    <div className="w-full h-screen p-10 md:px-16">
+      <div className="font-body">
         <h3 className="text-2xl text-green-400 font-display">Ashutosh</h3>
-        <h1 className="text-white text-5xl font-display">
+        <h1 className="text-5xl font-display">
           I build <br /> <span className="text-purple-400">applications</span>{" "}
           <br /> for web
         </h1>
@@ -20,23 +20,33 @@ const IndexPage = () => (
 
       <div className="mt-10 md:flex font-body">
         <div className="w-full md:w-1/2 py-2 md:px-4">
-          <p className="text-white text-xl font-display">
+          <p className=" text-xl font-display">
             A software engineer based in India who enjoys building websites and
             applications.
           </p>
         </div>
         <div className="w-full md:w-1/2 py-2 md:px-4">
-          <p className="text-white">
+          <p className="">
             See bits and pieces on GitHub, packages on npm, thoughts on Medium,
             and a full resume on LinkedIn
           </p>
           <div>
-            <ul className='flex ml-0'>
-              <li className='px-4'>
-                <a className='text-white hover:text-purple-400 border-b border-white hover:border-purple-700' href="https://github.com/ashu96">Github</a>
+            <ul className="flex ml-0">
+              <li className="px-4">
+                <a
+                  className=" hover:text-purple-400 border-b border-white hover:border-purple-700"
+                  href="https://github.com/ashu96"
+                >
+                  Github
+                </a>
               </li>
-              <li className='px-4'>
-                <a className='text-white hover:text-purple-400 border-b border-white hover:border-purple-700' href="https://github.com/ashu96">LinkedIn</a>
+              <li className="px-4">
+                <a
+                  className=" hover:text-purple-400 border-b border-white hover:border-purple-700"
+                  href="https://github.com/ashu96"
+                >
+                  LinkedIn
+                </a>
               </li>
             </ul>
           </div>
